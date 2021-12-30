aee674da6a501490df6697e429ce04da6731a9f9
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-30 15:40:14 -0800

    version1.0

--- proyecto10.9/proyecto10.9/Program.cs
+++ proyecto10.9/proyecto10.9/Program.cs
@@ -12,14 +12,23 @@ namespace proyecto10._9
         {
             try
             {
-                int numero, posicion;
-                Console.Write("Cual tabla quiere ver? ");
-                numero = int.Parse(Console.ReadLine());
-                for (int i = 0; i < 13; i++)
+                int numero = 0;
+                int posicion;
+                while (numero < 1 || numero > 10)
                 {
-                    posicion = i + 1;
-                    Console.WriteLine(posicion+" x "+numero+" = "+(posicion*numero));
+                    Console.Write("Cual tabla quiere ver? ");
+                    numero = int.Parse(Console.ReadLine());
                 }
+
+                if (numero >= 1 && numero <= 10)
+                {
+                    for (int i = 0; i < 13; i++)
+                    {
+                        posicion = i + 1;
+                        Console.WriteLine(posicion + " x " + numero + " = " + (posicion * numero));
+                    }
+                }
+                
             }
             catch (Exception)
             {
